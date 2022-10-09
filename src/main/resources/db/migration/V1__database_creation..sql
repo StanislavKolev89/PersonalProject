fd8d93e67b2cdf731202ab2ca829a86b6ed6ad16
kolev <kolevone@gmail.com>
2022-10-09 23:45:04 +0300

    preparing for deployment

--- src/main/resources/db/migration/V1__database_creation..sql
+++ src/main/resources/db/migration/V1__database_creation..sql
@@ -1,13 +1,13 @@
 create table roles
 (
-    id   bigint auto_increment
+    id   SERIAL
         primary key,
     name varchar(255) not null
 );
 
 create table categories
 (
-    id        bigint auto_increment
+    id        SERIAL
         primary key,
     deleted   bit          not null,
     image_url longtext     not null,
@@ -16,24 +16,24 @@ create table categories
 
 create table products
 (
-    id          bigint auto_increment
+    id          SERIAL
         primary key,
     deleted     bit            not null,
     description longtext       not null,
     image_url   longtext       not null,
     price       decimal(19, 2) not null,
     title       varchar(255)   not null,
-    category_id bigint         null,
+    category_id SERIAL         null,
     constraint FKog2rp4qthbtt2lfyhfo32lsw9
         foreign key (category_id) references categories (id)
 );
 
 create table warehouse
 (
-    id         bigint auto_increment
+    id         SERIAL
         primary key,
     quantity   int    not null,
-    product_id bigint null,
+    product_id SERIAL null,
     constraint FKow13o6v2o8btmca0nw5pblpss
         foreign key (product_id) references products (id)
 );
@@ -41,7 +41,7 @@ create table warehouse
 -- auto-generated definition
 create table users
 (
-    id         bigint auto_increment
+    id         SERIAL
         primary key,
     active     bit          not null,
     address    varchar(255) not null,
@@ -50,7 +50,7 @@ create table users
     last_name  varchar(255) not null,
     password   varchar(255) not null,
     username   varchar(255) not null,
-    role_id    bigint       null,
+    role_id    SERIAL       null,
     constraint UK_6dotkott2kjsp8vw4d0m25fb7
         unique (email),
     constraint UK_r43af9ap4edm43mmtq01oddj6
@@ -62,15 +62,15 @@ create table users
 -- auto-generated definition
 create table used_products
 (
-    id           bigint auto_increment
+    id           SERIAL
         primary key,
     description  longtext       not null,
     image_url    longtext       null,
     phone_number varchar(255)   not null,
     price        decimal(19, 2) not null,
     title        varchar(255)   not null,
-    category_id  bigint         null,
-    user_id      bigint         null,
+    category_id  SERIAL         null,
+    user_id      SERIAL         null,
     constraint FK6dudjpfumrokdha1x1fni9eor
         foreign key (category_id) references categories (id),
     constraint FKp0i6ypn5mpd5du3tbibrumvrj
@@ -80,11 +80,11 @@ create table used_products
 
 create table orders
 (
-    id         bigint auto_increment
+    id      SERIAL
         primary key,
     created_at datetime(6) not null,
     deleted    bit         not null,
-    user_id    bigint      null,
+    user_id    SERIAL      null,
     constraint FK32ql8ubntj5uh44ph9659tiih
         foreign key (user_id) references users (id)
 );
@@ -92,11 +92,11 @@ create table orders
 
 create table ordered_products
 (
-    id         bigint auto_increment
+    id         SERIAL
         primary key,
     quantity   int    not null,
-    order_id   bigint null,
-    product_id bigint null,
+    order_id   SERIAL null,
+    product_id SERIAL null,
     constraint FKi7isf670mbq331v0muqqry1cd
         foreign key (order_id) references orders (id),
     constraint FKn042ir6sf41qikoy2c07gt87i
@@ -106,12 +106,12 @@ create table ordered_products
 
 create table comments
 (
-    id         bigint auto_increment
+    id         SERIAL
         primary key,
     content    text        not null,
     created_at datetime(6) not null,
-    author_id  bigint      null,
-    product_id bigint      null,
+    author_id  SERIAL      null,
+    product_id SERIAL      null,
     constraint FK6uv0qku8gsu6x1r2jkrtqwjtn
         foreign key (product_id) references products (id),
     constraint FKn2na60ukhs76ibtpt9burkm27
